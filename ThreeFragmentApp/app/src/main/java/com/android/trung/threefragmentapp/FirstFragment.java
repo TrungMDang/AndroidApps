2c3d0c209251d1d0b7c5a512fabee6ffd36f1ce2
Trung Dang <tdang229@uw.edu>
2018-01-28 12:45:15 -0800

    loaded first fragment in main activity

--- ThreeFragmentApp/app/src/main/java/com/android/trung/threefragmentapp/FirstFragment.java
+++ ThreeFragmentApp/app/src/main/java/com/android/trung/threefragmentapp/FirstFragment.java
@@ -1,16 +1,63 @@
 package com.android.trung.threefragmentapp;
 
-import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by trung on 1/27/18.
  */
 
 public class FirstFragment extends Fragment {
+
+    /**
+     * Use to listen for item selected and communicate with parent activity.
+     */
+    private onItemSelected listener;
+
+    public interface onItemSelected {
+         boolean onButtonSelected(String text);
+    }
+
+    /**
+     * Guide:
+     * https://github.com/codepath/android_guides/wiki/Creating-and-Using-Fragments#fragment-listener
+     *
+     * @param context
+     */
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof onItemSelected) {
+            listener = (onItemSelected) context;
+            System.out.println("Listener created.");
+        } else {
+            throw new ClassCastException(context.toString() + "Must implement interface " +
+                    "FirstFragment.onItemSelected.");
+        }
+    }
+
+    public void onButtonSelected(View view) {
+        listener.onButtonSelected("button selected");
+        System.out.println("button selected");
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //TODO Receive data from 3rd fragment
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
--- ThreeFragmentApp/app/src/main/java/com/android/trung/threefragmentapp/MainActivity.java
+++ ThreeFragmentApp/app/src/main/java/com/android/trung/threefragmentapp/MainActivity.java
@@ -1,23 +1,32 @@
 package com.android.trung.threefragmentapp;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Constructor;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity implements FirstFragment.onItemSelected{
 
-
+    // Create a new Fragment to be placed in the activity layout
+    Fragment firstFragment = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+        //Look for json file and parse it into string
         String jsonIn = null;
         try {
             InputStream in = getAssets().open("data/first_frag.json");
@@ -30,13 +39,70 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
+        //Read json string
         try {
             JSONObject reader = new JSONObject(jsonIn);
-            System.out.println("Name:" + reader.getString("name"));
-            System.out.println("Classpath:" + reader.getString("classpath"));
+            String name = reader.getString("name");
+            String classpath = reader.getString("classpath");
+            System.out.println("Name:" + name);
+            System.out.println("Classpath:" + classpath);
+
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fm.beginTransaction();
+
+
+            /**
+             *  Source: android developer
+             *
+             */
+            // Check that the activity is using the layout version with
+            // the fragment_container FrameLayout
+            if (findViewById(R.id.frame_container) != null) {
+
+                // However, if we're being restored from a previous state,
+                // then we don't need to do anything and should return or else
+                // we could end up with overlapping fragments.
+                if (savedInstanceState != null) {
+                    return;
+                }
+
+
+                //Reflection
+                try {
+                    Class fragment = Class.forName(classpath);
+                    firstFragment = (Fragment) fragment.newInstance();
+
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                } catch (InstantiationException e) {
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+
+                // In case this activity was started with special instructions from an
+                // Intent, pass the Intent's extras to the fragment as arguments
+                firstFragment.setArguments(getIntent().getExtras());
+
+                // Add the fragment to the 'fragment_container' ConstraintLayout
+                fragmentTransaction.add(R.id.frame_container, firstFragment);
+                fragmentTransaction.commit();
+            }
 
         } catch (JSONException e) {
             e.printStackTrace();
         }
+
+
+    }
+
+    @Override
+    public boolean onButtonSelected(String text) {
+        if(firstFragment != null && firstFragment.isInLayout()) {
+            Toast.makeText(this, "button selected", Toast.LENGTH_SHORT).show();
+            System.out.println("Button selected Main activity");
+        }
+        return false;
     }
 }
