ba95e084de722f8d7040fa0c8115c5c369361270
DESKTOP-9S6VF61\Trung Dang <trungdang@tecace.com>
2018-03-05 17:17:16 -0800

    added preample annotation

---
+++ ThreeFragmentApp/app/src/main/java/com/android/trung/threefragmentapp/Preample.java
@@ -0,0 +1,13 @@
+package com.android.trung.threefragmentapp;
+
+/**
+ * Created by Trung Dang on 3/5/2018.
+ */
+
+@interface Preample {
+    String date();
+    String version();
+    String lastModified();
+    String modifiedBy();
+    String[] reviewers();
+}
